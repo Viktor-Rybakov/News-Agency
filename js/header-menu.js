5ae4446a7e1bf4420dc472812205ee23a4eacc0e
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-01-12 15:26:18 +0300

    Добавил: закрепление хедера при скролле анимацию кнопки закрытия меню выбора города Исправил: мобильное меню (теперь закрепляется внизу хедера)

---
+++ js/header-menu.js
@@ -0,0 +1,46 @@
+$(function(){
+  let fixedMenu = $('.js-header-middle');
+  let datetime = $('.js-datetime');
+  let weather = $('.js-weather');
+  let currentCity = $('.js-current-city');
+  let menuToTop = fixedMenu.offset().top;
+  let menuHeight = fixedMenu.innerHeight();
+  let menuToBottom = menuToTop + menuHeight;
+  let docWidth = $(window).width();
+  let main = $('.main');
+
+// Закрепление панели меню наверху при скролле
+
+  // $(window).scroll(function() {
+  //   if ( ($(window).scrollTop() > menuToTop) && (docWidth <= 768) ) {
+  //     fixedMenu.addClass('fixed');
+  //     main.css('margin-top', menuHeight);
+  //   } else {
+  //     fixedMenu.removeClass('fixed');
+  //     main.removeAttr('style');
+  //   }
+  // });
+
+  $(window).scroll(function() {
+    if ( ($(window).scrollTop() > menuToBottom) && (docWidth <= 768) ) {
+      fixedMenu.addClass('fixed');
+      fixedMenu.css({
+        "padding-bottom": "13px",
+        "background": "none",
+        "background-color": "#ffffff",
+      });
+      main.css('margin-top', menuHeight);
+      datetime.hide();
+      weather.hide();
+      currentCity.hide();
+    } else {
+      fixedMenu.removeClass('fixed');
+      fixedMenu.removeAttr('style');
+      main.removeAttr('style');
+      datetime.show();
+      weather.show();
+      currentCity.show();
+    }
+  });
+  
+});
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,39 +1,50 @@
 $(function(){
 
   let mobileMenuButton = $('.js-mobile-menu-button');
-  let mobileMenu = $('#menu');
+  let mobileMenu = $('.js-header-menu');
+
+// Открываем меню при клике на кнопку
 
   mobileMenuButton.children().addBack().on('click', function() {
-    mobileMenu.toggleClass('header-menu_mobile');
+    mobileMenu.toggleClass('modal');
+    let menuPosition = $('.js-header-middle').position().top + $('.js-header-middle').innerHeight();
 
     if ( mobileMenuButton.attr('aria-expanded') === 'false' ) {
       mobileMenuButton.attr('aria-label', 'Закрыть меню');
       mobileMenuButton.attr('aria-expanded', 'true');
-      mobileMenuButton.addClass('mobile-menu-button_open')
+      mobileMenuButton.addClass('mobile-menu-button_open');
+      mobileMenu.css('top', menuPosition);
     } else {
       mobileMenuButton.attr('aria-label', 'Открыть меню');
       mobileMenuButton.attr('aria-expanded', 'false');
       mobileMenuButton.removeClass('mobile-menu-button_open');
+      mobileMenu.removeAttr('style');
     }
   });
+
+// Закрываем меню, если ширина окна стала больше, чем 768 пикселей
 
   $(window).resize( function(){
     let docWidth = $(window).width();
 
     if (docWidth > 768) {
-      mobileMenu.removeClass('header-menu_mobile');
       mobileMenuButton.attr('aria-label', 'Открыть меню');
       mobileMenuButton.attr('aria-expanded', 'false');
       mobileMenuButton.removeClass('mobile-menu-button_open');
+      mobileMenu.removeClass('modal');
+      mobileMenu.removeAttr('style');
     }
   });
 
+// Закрываем меню при клике вне его
+
   $(document).click(function (e) {
     if ( !mobileMenuButton.children().addBack().is(e.target) && !mobileMenu.is(e.target) && mobileMenu.has(e.target).length === 0) {
-      mobileMenu.removeClass('header-menu_mobile');
       mobileMenuButton.attr('aria-label', 'Открыть меню');
       mobileMenuButton.attr('aria-expanded', 'false');
       mobileMenuButton.removeClass('mobile-menu-button_open');
+      mobileMenu.removeClass('modal');
+      mobileMenu.removeAttr('style');
     }
   });
 });
